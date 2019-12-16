d7e0e5d0805f22397bac5871d663b4a9c88e44e8
nurlopez <nlopezcolom@gmail.com>
2019-12-16 17:58:43 +0100

    Update router.get category

--- routes/resources.js
+++ routes/resources.js
@@ -1,33 +1,43 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
 
+const Resources = require("../models/Resources");
 
-const Resources = require('../models/Resources');
+// GET /resources
 
+router.get("/", (req, res, next) => {
+  Resources.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// GET /resources  
+// GET /resources/categories/letters
 
-router.get('/categories/:category', (req, res, next) => {
-  Resources.find() 
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-        res.json(err);
-      })
-  })
-  
-  
+router.get("/categories/:category", async (req, res, next) => {
+  const { category } = req.params;
+  console.log('in backend', req.params)
+  try {
+    const categoryResult = await Resources.find({ category });
+    res.status(200).json(categoryResult);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // / GET '/:Id'   => to retrieve a specific signcard
 // router.get('/:Id', (req, res, next) => {
 
-//     //we have to deconstruct from the params. 
+//     //we have to deconstruct from the params.
 //     const { Id } = req.params;
 // console.log('ups!',req.params)
 //     // if ( !mongoose.Types.ObjectId.isValid(signCardId)) {
 //     //     res.status(500).json({ message: 'Specified signCardId is invalid' });
 //     //     return;
-//     //   } 
+//     //   }
 
 //     Resources.findById( Id )
 //         .then (foundResource => {
@@ -38,9 +48,9 @@ router.get('/categories/:category', (req, res, next) => {
 //         })
 //     })
 
-  // GET /resources/category  
-  
-  // router.get('/:category', (req, res, next) => {
-  // });
-  
-  module.exports = router;
+// GET /resources/category
+
+// router.get('/:category', (req, res, next) => {
+// });
+
+module.exports = router;
